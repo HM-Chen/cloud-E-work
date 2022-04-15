7ecbb7012d22d393410873ad72e386b3ba857282
Homes <Homes@mai.com>
2022-04-15 20:12:25 +0800

    4.15 commit

--- yeb-server/src/main/java/com/xxxx/server/controller/TMenuController.java
+++ yeb-server/src/main/java/com/xxxx/server/controller/TMenuController.java
@@ -1,17 +1,22 @@
 package com.xxxx.server.controller;
 
 
+import com.xxxx.server.pojo.TAdmin;
 import com.xxxx.server.pojo.TMenu;
 import com.xxxx.server.service.ITAdminService;
 import com.xxxx.server.service.impl.TMenuServiceImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -28,11 +33,24 @@ import java.util.List;
 public class TMenuController {
     @Autowired
     private TMenuServiceImpl tMenuService;
+    @Autowired
+    private RedisTemplate<String,Object> redisTemplate;
 
     @ApiOperation(value = "通过用户id查询菜单列表")
     @GetMapping("/menu")
     private List<TMenu> getMenuByAdminId(){
-        return tMenuService.getMenusByAdminId();
+        Integer adminId = ((TAdmin)SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId();
+        ValueOperations<String,Object> valueOperations = redisTemplate.opsForValue();
+        List<TMenu> menus = (List<TMenu>)valueOperations.get("menu_"+adminId);
+        if(menus == null){
+            System.out.println("从数据库查询");
+            menus = tMenuService.getMenusByAdminId();
+            valueOperations.set("menu_"+adminId,menus);
+            redisTemplate.expire("menu_"+adminId,30000, TimeUnit.MILLISECONDS);
+        }else{
+            System.out.println("从redis查询");
+        }
+        return menus;
     }
 
 }
